fb685cc9c7edbaf5071d9da6b9ce5d66538342e1
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-05-30 15:38:47 -0400

    center card

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ mainEl.appendChild(h1);
 h1.style.position = 'absolute'
 h1.style.top = '0'
 
+
+const imagEl = document.createElement('img')
+imagEl.setAttribute("src" , 'https://i.pinimg.com/736x/41/d8/37/41d8375f3237702fed8b274ae68306ab.jpg');
+mainEl.appendChild(imagEl)
+imagEl.style.height = '400px'
+
