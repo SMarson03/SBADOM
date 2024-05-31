a6753e0a9d0250ef39b5f1ca1b3068c044a3a270
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-05-31 09:25:48 -0400

    still figuring it out

--- app.js
+++ app.js
@@ -10,20 +10,13 @@ h1.style.position = 'absolute'
 h1.style.top = '0'
 
 const cards = [
-    {name:"The Sun", URL:"https://biddytarot.com/wp-content/uploads/2018/02/19-sun-345x480.png"},
-    {name:"Ace of Cups", URL:"https://biddytarot.com/wp-content/uploads/2013/12/cups-01-ace-345x480.png"},
-    {name:"The Fool", URL:"https://biddytarot.com/wp-content/uploads/2018/02/0-fool-345x480.png"},
-    {name:"The Lovers", URL:"https://biddytarot.com/wp-content/uploads/2018/02/06-lovers-345x480.png"},
-    {name:"The World", URL:"https://biddytarot.com/wp-content/uploads/2018/02/21-world-345x480.png"},
-    {name:"The Tower", URL:"https://biddytarot.com/wp-content/uploads/2018/02/16-tower-345x480.png"},
-    {name:"Death", URL:"https://biddytarot.com/wp-content/uploads/2018/02/13-death-345x480.png"},
-    {name:"The SunD", URL:"https://biddytarot.com/wp-content/uploads/2018/02/19-sun.png"},
-    {name:"Ace of CupsD", URL:"https://biddytarot.com/wp-content/uploads/2013/12/cups-01-ace.png"},
-    {name:"The FoolD", URL:"https://biddytarot.com/wp-content/uploads/2018/02/0-fool.png"},
-    {name:"The LoversD", URL:"https://biddytarot.com/wp-content/uploads/2018/02/06-lovers.png"},
-    {name:"The WorldD", URL:"https://biddytarot.com/wp-content/uploads/2018/02/21-world.png"},
-    {name:"The TowerD", URL:"https://biddytarot.com/wp-content/uploads/2018/02/16-tower.png"},
-    {name:"DeathD", URL:"https://biddytarot.com/wp-content/uploads/2018/02/13-death.png"},
+    {name:"The Sun", URL:"https://biddytarot.com/wp-content/uploads/2018/02/19-sun-345x480.png", text: "The Sun represents success, radiance and abundance. The Sun gives you strength and tells you that no matter where you go or what you do, your positive and radiant energy will follow you and bring you happiness and joy. People are drawn to you because you can always see the bright side and bring such warmth into other people’s lives."},
+    {name:"Ace of Cups", URL:"https://biddytarot.com/wp-content/uploads/2013/12/cups-01-ace-345x480.png", text:""},
+    {name:"The Fool", URL:"https://biddytarot.com/wp-content/uploads/2018/02/0-fool-345x480.png", text:""},
+    {name:"The Lovers", URL:"https://biddytarot.com/wp-content/uploads/2018/02/06-lovers-345x480.png", text:""},
+    {name:"The World", URL:"https://biddytarot.com/wp-content/uploads/2018/02/21-world-345x480.png", text:""},
+    {name:"The Tower", URL:"https://biddytarot.com/wp-content/uploads/2018/02/16-tower-345x480.png", text:"Just when you think you’re safe and comfortable, a Tower moment hits and throws you for a loop. A lightning bolt of clarity and insight cuts through the lies and illusions you have been telling yourself, and now the truth comes to light. "},
+    {name:"Death", URL:"https://biddytarot.com/wp-content/uploads/2018/02/13-death-345x480.png", text:"After a period of pause and reflection with The Hanged Man, the Death card symbolizes the end of a major phase or aspect of your life that you realize is no longer serving you, opening up the possibility of something far more valuable and essential. "},
 ]
 
 function shuffle(cards){
@@ -38,6 +31,14 @@ const imagEl = document.createElement('img')
 imagEl.setAttribute("src" , 'https://i.pinimg.com/736x/41/d8/37/41d8375f3237702fed8b274ae68306ab.jpg');
 mainEl.appendChild(imagEl)
 imagEl.style.height = '400px'
+
+const pEl = document.createElement ('p')
+mainEl.appendChild(pEl)
+function createText(text){
+    const p = document.createElement("p")
+    p.src = text
+    return text
+}
  
 //Pulled the url from the array and created an image element
 //allowing each url pulled to show a face value on the site
@@ -50,16 +51,24 @@ function createImg(url){
 //Adding event listener to face down card that every time its clicked
 //you "fortune" is read by a new card. My main issue allowing and append once
 imagEl.addEventListener("click", (event)=>{
-    
+    mainEl.replaceChildren(mainEl.fourthChild)
     const backCard = shuffle(cards)[cards.length-1]
-    const {name, URL} = backCard
+    const {name, URL, text} = backCard
     const img = createImg(URL)
-    mainEl.appendChild(img)
+    const p = createText(text)
+    mainEl.appendChild(img, p)
     img.style.height = '400px'
+        console.log(img, text)
+    
   
-}, {once: true}, //I DID IT!! Ok so now I"m figuring out how to rest position and a form validation
-)
+}) 
 
 //bring it back to neutral
 
+// function validate(){
+// let num = document.myform.num.value;
+// if(isNaN(num)){
+//     document.getElementById("numloc")
+// }
 
+// }
